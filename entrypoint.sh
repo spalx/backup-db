2586934014f44f01b32de7035f788f8406f20998
Artavazd Barseghyan <artavazd1@gmail.com>
2026-03-24 22:37:18 +0100

    flag to disable cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ "$BKP_CRON_ENABLED" != "true" && "$BKP_CRON_ENABLED" != "1" ]]; then
+  echo "Cron is disabled (BKP_CRON_ENABLED=$BKP_CRON_ENABLED). Skipping setup."
+  exit 0
+fi
+
 if [ -z "$BKP_CRON_SCHEDULE" ]; then
   BKP_CRON_SCHEDULE="0 */12 * * *"
 fi
